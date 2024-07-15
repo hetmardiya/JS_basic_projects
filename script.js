c61077cd2512cdb7c9f0b997cc1f8dfd6ff4c48d
Het Mardiya <hetmardiya9@gmail.com>
2024-07-16 00:03:40 +0530

    get the value from js

---
+++ script.js
@@ -0,0 +1,6 @@
+let random_number = parseInt(Math.random() * (100 + 1))
+
+let submit = document.querySelector("#submit");
+let user_input = document.querySelector("#input_value");
+let remaining_attempt = document.querySelector(".attempt");
+let previous_number = document.querySelector(".previous");
